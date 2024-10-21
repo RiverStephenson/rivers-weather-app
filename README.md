fe17ece083d9dce6ea289d9f131c741619bbba2a
RiverStephenson <riverstephenson06@outlook.com>
2024-10-21 14:16:16 -0500

    added final touches

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # rivers-weather-app
+
+## Description
+
+This project will present you with a page where you may search any city and be presented with a 5-day forecast of the weather in said city. 
+
+## Installation
+
+Here is the GitHub link https://github.com/RiverStephenson/rivers-weather-app
+
+## Usage 
+
+## Credits
+
+The wonderful TAs (shoutout Mary) and Dan the Man 
+Link to the 5 day weather forecast website used:
+https://openweathermap.org/forecast5 
+
+## License
+
+The license used for this project was MIT https://opensource.org/license/MIT 
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,3 +1,5 @@
+const express = require("express");
+const router = express.Router();
 // TODO: Define a City class with name and id properties
 class city {
   cityName: string;
@@ -14,21 +16,39 @@ class city {
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  app.get('')
+  private async read() {
+
+  }
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+   try {
+     router.get('/:newCity', (_req: Request, res: Response) => {
+      res.json()
+    })
+   } catch (error) {
+    
+   }
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+try {
+  router.get()
+} catch (error) {
+  
+}
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {
     try {
-      
+     router.put('/:newCity', (_req: Request, res: Response) => {
+     res.json()
+     })
     } catch (error) {
       console.log(error);
       return error;
     }
-  }
+  };
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
